8ce8a3d18ed950cf3a0fec68542dcd19d8001b11
joopitz <jonas.opitz@live.de>
2023-07-27 10:38:07 +0200

    dev: add development environment (for rnix-lsp)

--- flake.nix
+++ flake.nix
@@ -1,16 +1,28 @@
 {
   description = "Nix Flake Templates";
 
-  outputs = { self, ... }: {
-    templates = {
-      python-package = {
-        path = ./python-package-template;
-        description = "A Python package defined as a Nix Flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      templates = {
+        python-package = {
+          path = ./python-package-template;
+          description = "A Python package defined as a Nix Flake";
+        };
+        python-application = {
+          path = ./python-application-template;
+          description = "A Python application defined as a Nix Flake";
+        };
       };
-      python-application = {
-        path = ./python-application-template;
-        description = "A Python application defined as a Nix Flake";
+      # the development environment
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [pkgs.rnix-lsp];
       };
     };
-  };
 }
