4902ae5dd453ef61786c6f10570b5a40c3d67113
jokatzke <jokatzke@fastmail.com>
2024-02-08 12:00:06 +0100

    feat: add system-independent overlay & lib

--- python-application-template/flake.nix
+++ python-application-template/flake.nix
@@ -1,104 +1,124 @@
 {
-  description = "A Python package defined as a Nix Flake";
+  description = "A Python application defined as a Nix Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          # enable if unfree packages are required
-          config.allowUnfree = false;
-        };
-        nix-filter = self.inputs.nix-filter.lib;
-        python = pkgs.python310;
+    let
+      nix-filter = self.inputs.nix-filter.lib;
 
-        ### list of python packages required to build / run the application
-        python-packages-build = py-pkgs:
-          with py-pkgs; [ pandas
-                          numpy
-                          # <add more packages here>
-                          
-                          /*
-                          dependencies from PyPi, generated through nix-init
-                          (pkgs.callPackage
-                            ./pkgs/<package name>.nix
-                            {inherit buildPythonPackage <package deps>;})
-                         */
-                        ];
-        
-        ### list of python packages to include in the development environment
-        # the development installation contains all build packages,
-        # plus some additional ones we do not need to include in production.
-        python-packages-devel = py-pkgs:
-          with py-pkgs; [ ipython
-                          jupyter
-                          black
-                          pyflakes
-                          isort
-                        ]
-          ++ (python-packages-build py-pkgs);
+      ### create the python installation for the package
+      python-packages-build = py-pkgs:
+        with py-pkgs; [
+          numpy
+          pandas
+          # <add more packages here>
+        ];
 
-        ### create the python package
-        python-app = python.pkgs.buildPythonApplication {
-          pname = "my-python-app";
-          version = "0.1.0";
-          /*
-          only include files that are related to the application
-          this will prevent unnecessary rebuilds
-          */
-          src = nix-filter {
-            root = self;
-            include = [
-              # folders
-              "src"
-              "my-python-app"
-              "test"
-              # files
-              ./setup.py
-              ./requirements.txt
-            ];
-            exclude = [ (nix-filter.matchExt "pyc") ];
-          };
-          propagatedBuildInputs = (python-packages-build python.pkgs);
-        };
-        
-        ### build the docker image
-        docker-img = pkgs.dockerTools.buildImage {
-          name = python-app.pname;
-          tag = python-app.version;
-          config = {
-            # name of command modified in setup.py
-            Cmd = ["${python-app}/bin/my-python-app"];
-            # uncomment if the container needs access to ssl certificates
-            # Env = [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
-          };
-        };
+      ### create the python installation for development
+      # the development installation contains all build packages,
+      # plus some additional ones we do not need to include in production.
+      python-packages-devel = py-pkgs:
+        with py-pkgs; [
+          black
+          ipython
+          isort
+          mypy
+          pyflakes
+          pylint
+          pytest
+          pytest-cov
+          # library stubs for mypy
+          pandas-stubs
+        ]
+        ++ (python-packages-build py-pkgs);
 
-      in rec {
-        # the packages that we can build
-        packages = rec {
-          my-python-app = python-app;
-          docker = docker-img;
-          default = my-python-app;
-        };
-        # the development environment
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            # the development installation of python
-            (python.withPackages python-packages-devel)
-            # python LSP server
-            pkgs.nodePackages.pyright
-            # for automatically generating nix expressions, e.g. from PyPi
-            pkgs.nix-template
-            pkgs.nix-init
+      ### the python package and application
+      get-python-package = py-pkgs: py-pkgs.buildPythonPackage {
+        pname = "my-python-package";
+        version = "0.1.0";
+        /* only include files that are related to the application.
+               this will prevent unnecessary rebuilds */
+        src = nix-filter {
+          root = self;
+          include = [
+            # folders
+            "src"
+            "my_python_package"
+            "test"
+            # files
+            ./setup.py
+            ./requirements.txt
           ];
+          exclude = [ (nix-filter.matchExt "pyc") ];
         };
-      }
-    );
+        propagatedBuildInputs = (python-packages-build py-pkgs);
+      };
+
+      get-python-app = py-pkgs: py-pkgs.toPythonApplication (
+        get-python-package py-pkgs
+      );
+
+    in
+    {
+      lib = {
+        default = get-python-package;
+      };
+      # define an overlay to the library to nixpkgs
+      overlays.default = (final: prev: {
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (python-final: python-prev: {
+            my-python-package = self.outputs.lib.default python-final;
+          })
+        ];
+      });
+    } //
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          python = pkgs.python3;
+          python-app = get-python-app python.pkgs;
+
+          ### the docker image
+          docker-img = pkgs.dockerTools.buildImage {
+            name = python-app.pname;
+            tag = python-app.version;
+            config = {
+              # name of command modified in setup.py
+              Cmd = [ "${python-app}/bin/my-python-app" ];
+              # uncomment if the container needs access to ssl certificates
+              # Env = [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
+            };
+          };
+        in
+        {
+          # the packages that we can build
+          packages = {
+            default = python-app;
+          } // (nixpkgs.lib.optionalAttrs
+            # only build docker images on linux systems
+            (system == "x86_64-linux" || system == "aarch64-linux")
+            {
+              docker = docker-img;
+            }
+          );
+          # the development environment
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              # the development installation of python
+              (python.withPackages python-packages-devel)
+              # python lsp server
+              pkgs.nodePackages.pyright
+              # for automatically generating nix expressions, e.g. from PyPi
+              pkgs.nix-template
+              pkgs.nix-init
+            ];
+          };
+        }
+      );
 }
--- python-package-template/flake.nix
+++ python-package-template/flake.nix
@@ -8,84 +8,93 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # import the packages from nixpkgs
-        pkgs = import nixpkgs {
-          inherit system;
-          # enable if unfree software is required
-          config.allowUnfree = false;
-        };
-        nix-filter = self.inputs.nix-filter.lib;
-        # the python version we are using
-        python = pkgs.python310;
+    let
+      nix-filter = self.inputs.nix-filter.lib;
 
-        ### create the python installation for the package
-        python-packages-build = py-pkgs:
-          with py-pkgs; [ pandas
-                          numpy
-                          # <add more packages here>
-                          
-                          # dependencies from PyPi, generated through nix-init
-                          # (pkgs.callPackage
-                          #   ./pkgs/<package name>.nix
-                          #   {inherit buildPythonPackage <package dependencies>;})
-                        ];
+      ### create the python installation for the package
+      python-packages-build = py-pkgs:
+        with py-pkgs; [
+          numpy
+          pandas
+          # <add more packages here>
+        ];
 
-        ### create the python installation for development
-        # the development installation contains all build packages,
-        # plus some additional ones we do not need to include in production.
-        python-packages-devel = py-pkgs:
-          with py-pkgs; [ ipython
-                          jupyter
-                          black
-                          pyflakes
-                          isort
-                        ]
-          ++ (python-packages-build py-pkgs);
+      ### create the python installation for development
+      # the development installation contains all build packages,
+      # plus some additional ones we do not need to include in production.
+      python-packages-devel = py-pkgs:
+        with py-pkgs; [
+          black
+          ipython
+          isort
+          mypy
+          pyflakes
+          pylint
+          pytest
+          pytest-cov
+          # library stubs for mypy
+          pandas-stubs
+        ]
+        ++ (python-packages-build py-pkgs);
 
-        ### create the python package
-        python-package = python.pkgs.buildPythonPackage {
-          pname = "my-python-package";
-          version = "0.1.0";
-          /*
-          only include files that are related to the application
-          this will prevent unnecessary rebuilds
-          */
-          src = nix-filter {
-            root = self;
-            include = [
-              # folders
-              "src"
-              "my_python_package"
-              "test"
-              # files
-              ./setup.py
-              ./requirements.txt
-            ];
-            exclude = [ (nix-filter.matchExt "pyc") ];
-          };
-          propagatedBuildInputs = (python-packages-build python.pkgs);
-        };
-
-      in rec {
-        # the packages that we can build
-        packages = rec {
-          my-python-package = python-package;
-          default = my-python-package;
-        };
-        # the development environment
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            # the development installation of python
-            (python.withPackages python-packages-devel)
-            # python lsp server
-            pkgs.nodePackages.pyright
-            # for automatically generating nix expressions, e.g. from PyPi
-            pkgs.nix-template
-            pkgs.nix-init
+      ### create the python package
+      get-python-package = py-pkgs: py-pkgs.buildPythonPackage {
+        pname = "my-python-package";
+        version = "0.1.0";
+        /* only include files that are related to the application.
+               this will prevent unnecessary rebuilds */
+        src = nix-filter {
+          root = self;
+          include = [
+            # folders
+            "src"
+            "my_python_package"
+            "test"
+            # files
+            ./setup.py
+            ./requirements.txt
           ];
+          exclude = [ (nix-filter.matchExt "pyc") ];
         };
-      }
-    );
+        propagatedBuildInputs = (python-packages-build py-pkgs);
+      };
+    in
+    {
+      lib = {
+        default = get-python-package;
+      };
+      # define an overlay to the library to nixpkgs
+      overlays.default = (final: prev: {
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (python-final: python-prev: {
+            my-python-package = self.outputs.lib.default python-final;
+          })
+        ];
+      });
+    } //
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          python = pkgs.python3;
+        in
+        {
+          # the packages that we can build
+          packages = {
+            default = self.outputs.lib.default python.pkgs;
+          };
+          # the development environment
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              # the development installation of python
+              (python.withPackages python-packages-devel)
+              # python lsp server
+              pkgs.nodePackages.pyright
+              # for automatically generating nix expressions, e.g. from PyPi
+              pkgs.nix-template
+              pkgs.nix-init
+            ];
+          };
+        }
+      );
 }
