859f6cf7745597927ffb20ddca459dee682f6c6b
jokatzke <jokatzke@fastmail.com>
2024-04-17 12:18:29 +0200

    fix: realize symbolic links

--- python-package-template/python-lib.nix
+++ python-package-template/python-lib.nix
@@ -1,1 +1,42 @@
-../python-application-template/python-lib.nix
+# this is where we define the python package and all of its dependencies.
+{
+  nix-filter,
+  buildPythonPackage,
+  pytestCheckHook,
+  # example python packages
+  numpy,
+  pandas,
+}:
+buildPythonPackage {
+  pname = "my-python-package";
+  version = "0.1.0";
+  
+  # only include files that are related to the package.
+  # this will prevent unnecessary rebuilds
+  src = nix-filter {
+    root = ./.;
+    include = [
+      # folders
+      "src"
+      "my_python_package"
+      "test"
+      # files
+      ./setup.py
+      ./requirements.txt
+    ];
+    exclude = [ (nix-filter.matchExt "pyc") ];
+  };
+
+  # the python packages we use
+  propagatedBuildInputs = [
+    numpy
+    pandas
+  ];
+  
+  # if test through pytest are included, uncomment the following.
+  # this section is also where you would add additional test dependencies, such
+  # as hypothesis.
+  # nativeCheckInputs = [
+  #   pytestCheckHook
+  # ];
+}
--- python-package-template/shell.nix
+++ python-package-template/shell.nix
@@ -1,1 +1,42 @@
-../python-application-template/shell.nix
+# this is where we define the development environment, which includes all of
+# the package's dependencies and some additional packages we might want to
+# include.
+{
+  mkShell,
+  python3,
+  pyright,
+  nix-template,
+  nix-init,
+  nix-tree,
+  dive,
+}:
+mkShell {
+  packages = [
+    (python3.withPackages (
+      py-pkgs:
+      with py-pkgs;
+      [
+        # additional python packages we want to include in the development
+        # environment
+        black
+        ipython
+        jupyter
+        isort
+        mypy
+        pyflakes
+        pylint
+        pytest
+        pytest-cov
+      ]
+      # TODO: change the name to the actual package name
+      ++ py-pkgs.my-python-package.propagatedBuildInputs
+    ))
+    # python LSP
+    pyright
+    # tools for working with nix / docker
+    nix-template
+    nix-init
+    nix-tree
+    dive
+  ];
+}
